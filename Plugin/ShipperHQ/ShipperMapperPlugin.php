7774ec34950e56686039eb4c3a208961d41f4938
John Sparwasser <johnspar1@gmail.com>
2017-03-08 12:19:24 -0500

    More work on the plugin to send hardcoded shipping code to shipperhq

--- Plugin/ShipperHQ/ShipperMapperPlugin.php
+++ Plugin/ShipperHQ/ShipperMapperPlugin.php
@@ -6,12 +6,14 @@ class ShipperMapperPlugin
 {
     public function aroundPopulateAttributes(\ShipperHQ\Shipper\Model\Carrier\Processor\ShipperMapper $mapper, callable $proceed, ...$args)
     {
-        $returnValue = $proceed(...$args);
-        if (is_array($returnValue)) {
-            $returnValue[] = ['name' => 'shipperhq_shipping_group', 'value' => true];
+        $attributes = $proceed(...$args);
+        if (is_array($attributes)) {
+            foreach($attributes as $attribute) {
+                if (isset($attribute['name']) && $attribute['name'] === 'shipperhq_shipping_group') {
+                    $attribute['value'] .= '#SUBSCRIBEPRO_RECURRING';
+                }
+            }
         }
-        return $returnValue;
+        return $attributes;
     }
 }
-
-
