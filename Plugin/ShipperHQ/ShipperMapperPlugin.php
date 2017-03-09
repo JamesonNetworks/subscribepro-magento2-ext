a1e78bf0db9ad1b0f2d9b39f4c9e32b5d51b7f15
John Sparwasser <johnspar1@gmail.com>
2017-03-09 14:23:53 -0500

    Finished this phase

--- Plugin/ShipperHQ/ShipperMapperPlugin.php
+++ Plugin/ShipperHQ/ShipperMapperPlugin.php
@@ -2,20 +2,59 @@
 
 namespace Swarming\SubscribePro\Plugin\ShipperHQ;
 
+use Swarming\SubscribePro\Model\Quote\SubscriptionOption\OptionProcessor;
+use Swarming\SubscribePro\Api\Data\SubscriptionOptionInterface;
+use Swarming\SubscribePro\Api\Data\ProductInterface as PlatformProductInterface;
+
 class ShipperMapperPlugin
 {
     public function aroundPopulateAttributes(\ShipperHQ\Shipper\Model\Carrier\Processor\ShipperMapper $mapper, callable $proceed, $reqdAttributeNames, $item)
     {
-        log(get_class($item));
-        log(json_encode($item));
         $attributes = $proceed($reqdAttributeNames, $item);
-        if (is_array($attributes)) {
-            foreach($attributes as $attribute) {
-                if (isset($attribute['name']) && $attribute['name'] === 'shipperhq_shipping_group') {
-                    $attribute['value'] .= '#SUBSCRIBEPRO_RECURRING';
+
+        if ($this->determineIfSubscription($item)) {
+            $shippingGroup = '';
+            $shippingGroupSet = false;
+
+            $recurringShippingCode = 'SUBSCRIBEPRO_RECURRING';
+
+            if (is_array($attributes)) {
+                foreach($attributes as $key => $attribute) {
+                    if (isset($attribute['name']) && $attribute['name'] === 'shipperhq_shipping_group') {
+                        $shippingGroup = $attribute['value'] . '#' . $recurringShippingCode;
+                        $shippingGroupSet = true;
+                        $shippingKey = $key;
+                    }
+                }
+                if (!$shippingGroupSet) {
+                    $attributes[] = ['name' => 'shipperhq_shipping_group', 'value' => $recurringShippingCode];
+                } else {
+                    $attributes[$shippingKey] = ['name' => 'shipperhq_shipping_group', 'value' => $shippingGroup];
                 }
             }
         }
         return $attributes;
     }
+
+    private function determineIfSubscription($item)
+    {
+        $buyRequest = $item->getOptionByCode('info_buyRequest');
+        if (!$buyRequest) {
+            return false;
+        }
+        $buyRequest = unserialize($buyRequest->getValue());
+
+        if (!isset($buyRequest[OptionProcessor::KEY_SUBSCRIPTION_OPTION])) {
+            return false;
+        }
+
+        $subscriptionOptions = $buyRequest[OptionProcessor::KEY_SUBSCRIPTION_OPTION];
+
+        if (!isset($subscriptionOptions[SubscriptionOptionInterface::OPTION])) {
+            return false;
+        }
+
+        return $subscriptionOptions[SubscriptionOptionInterface::OPTION] == PlatformProductInterface::SO_SUBSCRIPTION;
+    }
 }
+
